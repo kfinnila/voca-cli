71e3d47230e8351eb58db95b32e247c18a1098d1
Kari Finnilä <Kari.Finnila@fetesa.red.com.ni>
2017-04-17 14:29:34 -0600

    Menu 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { MenuComponent } from './shared/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
